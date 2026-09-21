ae9076933bacb6566319a41b61e9c983f886b8a9
KL <obimbolemuel@gmail.com>
2026-09-22 01:06:03 +0300

    Sentence thing fixed , equals operation and if else sarcasm.

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
-print ("Hello whats your name? ")
-name= input()
-print("Nice to meet you")
-print(name)
+name= input ("Hello whats your name? ")
+print("Nice to meet you", name,"!")
+weather= input("whats the weather like today?")
+if weather == "sunny":
+    print("Go touch some grass", name,"!")
+else:
+    print("lets code some more", name,".")
